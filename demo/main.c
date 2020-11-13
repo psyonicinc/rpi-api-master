2c8b8f2bf678b77e352b341670eab7f330b80644
ocanath_robotman <ocanath@gmail.com>
2020-11-13 00:23:59 +0000

    init commit quickcheck

--- demo/main.c
+++ demo/main.c
@@ -247,6 +247,16 @@ void main()
 
 	open_i2c(0x50);	//Initialize the I2C port. Currently default setting is 100kHz clock rate
 
+	while(1)
+	{
+		if(read(file_i2c, i2c_rx_buf, 8) != 8)
+			ret |= (1 << 1);
+		printf("d = ");
+		for(int i =0; i < 8; i++)
+			printf("[%d]", i2c_rx_buf[i]);
+		printf("\r\n");
+	}
+
 	FILE * fp = fopen("datalog.csv", "w");
 	struct timeval tv;
 	
