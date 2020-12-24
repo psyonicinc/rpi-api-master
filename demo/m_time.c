f64609c4fd9d77b4ffb544e1f1ead0741c889b01
Dmitry Ilchenko <dmitryi2@illinois.edu>
2020-12-24 21:07:48 +0000

    updated m_time to the working version

--- demo/m_time.c
+++ demo/m_time.c
@@ -1,20 +1,28 @@
 #include "m_time.h"
 #include <string.h>
-
+#include <time.h>
 /*Setpoint generation start time*/
-struct timeval tv;
-struct timeval start_time;
+//struct timeval tv;
+//struct timeval start_time;
 
+static long ms;
+static time_t s;
+static struct timespec spec;
+static uint64_t start_time = 0;
 
-int64_t current_time_us(void)
+uint64_t current_time_us(void)
 {
-	gettimeofday(&tv,NULL);
-	return ( (tv.tv_sec - start_time.tv_sec)*1000000 + (tv.tv_usec - start_time.tv_usec) );
+	clock_gettime(CLOCK_BOOTTIME, &spec);
+	s = spec.tv_sec;
+	
+	return (s * (uint64_t)1000000 + (spec.tv_nsec/1.0e3)) - start_time;
+//	gettimeofday(&tv,NULL);
+//	return ( (tv.tv_sec - start_time.tv_sec)*((uint64_t)1000000) + (tv.tv_usec - start_time.tv_usec) );
 }
 
-float current_time_sec(void)
+double current_time_sec(void)
 {
-	return ((float)current_time_us())/1000000.0f;
+	return ((double)current_time_us())/1000000.0;
 }
 
 uint64_t get_tick()
@@ -24,5 +32,6 @@ uint64_t get_tick()
 
 void m_time_init(void)
 {
-	gettimeofday(&start_time, NULL);
+	start_time = current_time_us();
+//	gettimeofday(&start_time, NULL);
 }
