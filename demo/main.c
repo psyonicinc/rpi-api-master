fd8b3bc1c251382078af664313a87dcd10d286b3
Dmitry Ilchenko <dmitryi2@illinois.edu>
2021-01-04 17:50:39 +0000

    broken (doesn't compile) version of what i believe to be the most up to date disconnection tester by dmitry

--- demo/main.c
+++ demo/main.c
@@ -1,152 +1,298 @@
 #include "i2c-master-test.h"
 #include "i2c-err-lookup.h"
-#include "sig-catch.h"
-#include <stdio.h>
-#include "m_time.h"
 
-static struct timeval tv;
+//#define PRINT_PRESSURE	/*When enabled, prints the value of the pressure sensors on the index finger. */
+#define CABLE_TEST
+//#define PRINT_POSITION	/*When enabled, prints the finger position in degrees/*
 
-enum {PROGRAM_START, HAND_OPEN, HAND_POWER_GRASP};
-
-/*Special type for sending grip commmands over i2c*/
-typedef struct grip_cmd_t
+float current_time_sec(struct timeval * tv)
 {
-	uint8_t header;
-	uint8_t grip;
-	uint8_t speed;
-}grip_cmd_t;
+	gettimeofday(tv,NULL);
+	int64_t t_int = (tv->tv_sec*1000000+tv->tv_usec);
+	return ((float)t_int)/1000000.0f;
+}
 
+const char * finger_name[] = {
+	"index",
+	"middle",
+	"ring",
+	"pinky",
+	"thumb flex",
+	"thumb rot",
+};
 
-/*Write a grip command struct over i2c (3 bytes)*/
-int write_i2c_grip(grip_cmd_t grip)
+/**/
+void wait_for_cooldown(uint8_t * disabled_stat, float_format_i2c * out, float_format_i2c * in, pres_union_fmt_i2c * pres)
 {
-	uint8_t buf[3];
-	buf[0] = grip.header;
-	buf[1] = grip.grip;
-	buf[2] = grip.speed;
-	if(write(file_i2c, buf, 3) != 3)
-		return -1;
-	else
-		return 1;
+	while(*disabled_stat != 0)
+	{
+		for(int ch = 0; ch < NUM_CHANNELS; ch++)
+		{
+			out->v[ch] = 0.f;
+			int chk = (*disabled_stat >> ch) & 1;
+			if(chk)
+				printf("[%s cooling]", finger_name[ch]);
+		}
+		printf("\r\n");
+		int rc = send_recieve_floats(TORQUE_CTL_MODE, out, in, disabled_stat, pres);
+	}
 }
 
 void main()
-{
+{	FILE *log; //log file init of file and cleaning
+	time_t now;
+	int clock, secsPastMidnight, hours, minutes, seconds;
+	log = fopen("log.txt","a");
+	time(&now);
+			clock = now - 18000;
+			secsPastMidnight = clock % 86400;
+		hours = (secsPastMidnight / 3600)-1;
+		if (hours == -1)	hours = 23;
+			minutes = (secsPastMidnight % 3600) / 60;
+			seconds = secsPastMidnight % 60;
+	fprintf(log, "%02d:%02d:%02d Beginning of the test \n", hours, minutes, seconds);
+	printf("%02d:%02d:%02d Beginning of the test \n", hours, minutes, seconds);
+	fclose(log);
+
 	open_i2c(0x50);	//Initialize the I2C port. Currently default setting is 100kHz clock rate
-	m_time_init();
-	signal_catch_setup();
-	
-	FILE * tlog_fp = NULL;
-	tlog_fp = fopen("log.txt", "a");
-	fseek(tlog_fp, 0, SEEK_END);
-	
-	uint8_t i2c_rx_buf[8];	//for reading over i2c
 
-	
-	float next_state_ts = 1.f;	//used to schedule the next state transition
-	int state = PROGRAM_START;	//current state. 
-	
-	uint64_t cycle_count = 0;
-	uint8_t cycle_chk_ready_flag = 0;
-	float program_hung_ts = 20.f;
-	
-	printf("Program begin.\r\n");
-	fprintf(tlog_fp, "Program begin.\r\n");
-	float tstart = current_time_sec();	//timestamp to mark program start time
-	printf("start time: %f\r\n", tstart);
-	fprintf(tlog_fp, "start time: %f\r\n", tstart);
-	
-	while(gl_run_ok)
+	/*Quick example of pre-programmed grip control (i.e. separate control mode from torque, velocity and position control)*/
+	set_grip(GENERAL_OPEN_CMD,100);
+	usleep(2000000);
+	set_grip(POWER_GRASP_CMD,100);
+	usleep(2000000);
+	set_grip(GENERAL_OPEN_CMD,100);
+	usleep(2000000);
+
+	/*Setpoint generation start time*/
+	struct timeval tv;
+
+		int connect_flag[5];
+		for (int cnt = 0; cnt<5; cnt++)
+			connect_flag[cnt]=1;
+		int prev_phase = 0;
+		int phase = 0;
+		uint32_t cable_data[10];
+		uint32_t old_cable_data[10];
+		uint32_t gl_fall_diff = 0;
+		uint32_t gl_rise_diff = 0;
+		for (int i=0; i<10; ++i){
+			cable_data[i]=0;
+			old_cable_data[i]=0;
+		}
+
+	/*All control modes will use the same float format struct for input and output. Initializing them here*/
+
+	/*Setup for demo motion*/
+	uint8_t disabled_stat = 0xFF;
+
+	float qd[NUM_CHANNELS];
+	pres_union_fmt_i2c pres_fmt;
+	float_format_i2c i2c_out;
+	for(int ch = 0; ch < NUM_CHANNELS; ch++)
+		i2c_out.v[ch] = 0;
+	float_format_i2c i2c_in;
+	int rc=7;
+	int state[NUM_CHANNELS]={0};
+	unsigned int counter[NUM_CHANNELS]={0};
+
+	set_mode(TORQUE_CTL_MODE);
+	printf("prepping api...\r\n");
+	while(rc != 0)
 	{
-		float t = current_time_sec()-tstart;
-		int comms_stat = 0;	//0 denotes ok comms
-		
-		/*Read the hand i2c data*/
-		if(read(file_i2c, i2c_rx_buf, 8) == 8)
-		{}
-		else
-			comms_stat |= 1 << 1;
-
-		/*Manage what state happens next and when (relative to the execution of the current state switch). 
-		Statements wrapped in these cases get called once*/
-		if(t > next_state_ts)
-		{
-			switch(state)
-			{
-				case PROGRAM_START:	//program start has ENDED. transition action to next state...
-				{
-					if(write_i2c_grip((grip_cmd_t){0x1D, 0x00, 0xEF}) != 1)	//transistion to hand_open and send the open signal
-						comms_stat |= 1;
-					state = HAND_OPEN;
-					next_state_ts = t+=.3f;
-					break;
-				}
-				case HAND_OPEN:
-				{
-					if(i2c_rx_buf[7] == 0x17)
-					{
-						fprintf(tlog_fp, ", hand opened OK\r\n");
-						printf(", hand opened OK\r\n");
-					}
-					else
-					{
-						fprintf(tlog_fp, ", bad open\r\n");
-						printf(", bad open\r\n");
-					}
-					fclose(tlog_fp);
-					if(write_i2c_grip((grip_cmd_t){0x1D, 0x01, 0xEF}) != 1)	//transition to hand_power_grasp and close the hand
-						comms_stat |= 1;
-					state = HAND_POWER_GRASP;
-					next_state_ts = t + 3.f;
-					break;
-				}
-				case HAND_POWER_GRASP:
-				{		
-					tlog_fp = fopen("log.txt", "a");
-					fseek(tlog_fp, 0, SEEK_END);
-					
-					if(write_i2c_grip((grip_cmd_t){0x1D, 0x00, 0xEF}) != 1)	//transition to hand_open and open the hand
-						comms_stat |= 1;
-					state = HAND_OPEN;
-					cycle_chk_ready_flag = 1;
-					next_state_ts = t + 3.f;
-					break;
-				}
-				default:
-					break;
-			};
-			//statements not wrapped in the switch-case get called every state transition
-			if(comms_stat != 0)
-				print_hr_errcode(comms_stat);
-		}
-
-		/*Manage what happens while in the current state
-		Statements wrapped in these cases get called continuously,
-		while in the relevant state.*/
-		switch(state)
-		{
-			case(HAND_OPEN):
-			{
-				//if(comms_stat == 0 && i2c_rx_buf[7] == 0x17 && cycle_chk_ready_flag == 1)	//power grasp
-				if(comms_stat == 0 && cycle_chk_ready_flag == 1)
-				{
-					program_hung_ts = t + 20.f;
-					cycle_chk_ready_flag = 0;
-					cycle_count++;
-					printf("count = %llu, time = %f", cycle_count, t);
-					fprintf(tlog_fp, "count = %llu, time = %f", cycle_count, t);
-				}
-				break;
-			}
-			default:
-				break;
-		};		
-		if(t > program_hung_ts)
-		{
-			printf("error. hand has not cycled for 20 seconds\r\n");
-			break;
-		}
+		rc = send_recieve_floats(TORQUE_CTL_MODE, &i2c_out, &i2c_in, &disabled_stat, &pres_fmt);
+		if(rc != 0)
+			printf("waiting...\r\n");
 	}
-	printf("exiting program\r\n");
-	fclose(tlog_fp);
+	printf("api entered\r\n");
+	printf("disabling pressure HPF...\r\n");
+	if(set_mode(DISABLE_PRESSURE_FILTER) == 0)//example of pres filter disable
+		printf("pressure filter disabled\r\n");
+	else
+		printf("comm failure, filter not disabled\r\n");
+	usleep(3000000);	//delay for printf visibility
+	printf("waiting for motor cooldown\r\n");
+	wait_for_cooldown(&disabled_stat, &i2c_out, &i2c_in, &pres_fmt);
+	printf("ready\r\n");
+
+	float start_ts = current_time_sec(&tv);
+	float tau_thresh[NUM_CHANNELS] = {0};
+	while(1)
+	{
+		if(rc == 0)
+		{
+			float t = current_time_sec(&tv)-start_ts;
+			for(int ch = 0; ch < NUM_CHANNELS; ch++)
+			{
+				float qd = 105.f*(.5f*sin(2.f*t+(float)(5-ch)*3.1415f/6)+.5f) + 5.f;
+				if(ch == THUMB_ROTATOR)
+					qd = -qd;
+
+				/*Create a cooldown handler rule (stop the finger if it has triggered the 'hot' flag*/
+				int chk = (disabled_stat >> ch) & 1;
+				if(chk)
+				{
+					tau_thresh[ch] = 0.f;
+					if (ch == 2)
+					printf("[%s hot]", finger_name[ch]);
+				}
+				else
+					tau_thresh[ch] = 110.f;
+				/*Perform torque based position control*/
+				float tau = 1.f*(qd-i2c_in.v[ch]);
+				//printf ("%f \n", tau);
+				if(tau > tau_thresh[ch])
+					tau = tau_thresh[ch];
+				else if(tau < -tau_thresh[ch])
+					tau = -tau_thresh[ch];
+				i2c_out.v[ch] = tau;
+				if (tau < -0.5) state[ch] = 3;
+				if (tau > 0.5) state[ch] = 1;
+				if (state[ch] == 1 && tau >= -0.5 && tau <= 0.5){
+					state[ch] = 2;
+					counter[ch]++;
+				}
+				if (state[ch] == 3 && tau >= -0.5 && tau <= 0.5) state[ch] = 0;
+
+				//printf("%d\n", counter);
+
+			}
+		//	printf("\r\n");
+
+			/*
+			Pressure Indices:
+			Index: 	0-3
+			Middle: 4-7
+			Ring: 	8-11
+			Pinky: 	12-15
+			Thumb: 	16-19
+
+			Note that the The pressure range is NOT normalized (i.e. will range from 0-0xFFFF).
+			*/
+
+			#ifdef CABLE_TEST
+
+				int i, pidx;
+				for(pidx = 0; pidx < 19; pidx+=2){
+						cable_data[pidx/2]=((uint32_t)pres_fmt.v[pidx+1] << 16) +  pres_fmt.v[pidx];
+						//printf("%x ", cable_data[pidx/2]);
+				}
+				//printf("\n");
+
+				for (i = 0; i <10; i+=2){
+						int finger = i/2;
+						gl_fall_diff = cable_data[i]-old_cable_data[i];
+						gl_rise_diff = cable_data[i+1]-old_cable_data[i+1];
+						if (gl_fall_diff>0x1000 || gl_rise_diff> 0x1000)
+						  {//printf("Glitch");
+
+							old_cable_data[i] = cable_data [i];
+							old_cable_data[i+1] = cable_data[i+1];
+							gl_fall_diff = cable_data[i]-old_cable_data[i];
+							gl_rise_diff = cable_data[i+1]-old_cable_data[i+1];}
+
+						// else if (gl_fall_diff!=0 || gl_rise_diff!=0){
+						// 	printf("Fall %d\n", gl_fall_diff);
+						// 	printf("Rise %d\n", gl_rise_diff);
+						// 	old_cable_data[i] = cable_data [i];
+						// 	old_cable_data[i+1] = cable_data[i+1];
+						// 	gl_fall_diff = cable_data[i]-old_cable_data[i];
+						// 	gl_rise_diff = cable_data[i+1]-old_cable_data[i+1];
+						// }
+
+						if (gl_fall_diff>0&&connect_flag[finger]==1){
+							time(&now);
+		    					clock = now - 18000;
+		    					secsPastMidnight = clock % 86400;
+		   					hours = (secsPastMidnight / 3600)-1;
+								if (hours == -1)	hours = 23;
+		    					minutes = (secsPastMidnight % 3600) / 60;
+		    					seconds = secsPastMidnight % 60;
+							connect_flag[finger] = 0;
+					 		log = fopen("log.txt","a");
+					 		fprintf(log, "%02d:%02d:%02d ", hours, minutes, seconds);
+							fprintf(log, "Cycle %d ", counter[finger]);
+					 		if (state[finger]==1)
+					 		 {fprintf(log, "Finger %d disconnected during closing\n", finger+1);
+								//printf("Finger %d disconnected during closing\n", finger+1);
+								}
+					 		else if (state[finger]==3)
+					 			{fprintf(log, "Finger %d disconnected during opening\n", finger+1);
+								//printf("Finger %d disconnected during opening\n", finger+1);
+								}
+							else if (state[finger]==0)
+								{fprintf(log, "Finger %d disconnected while open \n", finger+1);
+								//printf("Finger %d disconnected while open \n", finger+1);
+								}
+							else
+							{fprintf(log, "Finger %d disconnected while closed \n", finger+1);
+							//printf("Finger %d disconnected while closed \n", finger+1);
+							}
+					 		fclose(log);
+							old_cable_data[i]=cable_data[i];
+							old_cable_data[i+1]=cable_data[i+1];
+						}
+
+						else if (gl_rise_diff>0&&connect_flag[finger]==0){
+							connect_flag[finger] = 1;
+					 		log = fopen("log.txt","a");
+							time(&now);
+		    					clock = now - 18000;
+		   					secsPastMidnight = clock % 86400;
+		    					hours = (secsPastMidnight / 3600)-1;
+									if (hours == -1)	hours = 23;
+		    					minutes = (secsPastMidnight % 3600) / 60;
+		    					seconds = secsPastMidnight % 60;
+							fprintf(log, "%02d:%02d:%02d ", hours, minutes, seconds);
+							fprintf(log, "Cycle %d ", counter[finger]);
+					 		if (state[finger]==1)
+					 		 {fprintf(log, "Finger %d connected during closing\n", finger+1);
+								//printf("Finger %d connected during closing\n", finger+1);
+								}
+					 		else if (state[finger]==3)
+					 			{fprintf(log, "Finger %d connected during opening\n", finger+1);
+								//printf("Finger %d connected during opening\n", finger+1);
+								}
+							else if (state[finger]==0)
+								{fprintf(log, "Finger %d connected while open \n", finger+1);
+								//printf("Finger %d connected while open \n", finger+1);
+								}
+							else
+							{fprintf(log, "Finger %d connected while closed \n", finger+1);
+							//printf("Finger %d connected while closed \n", finger+1);
+							}
+	 				 		fclose(log);
+							old_cable_data[i]=cable_data[i];
+							old_cable_data[i+1]=cable_data[i+1];
+						}
+
+				}
+
+			#endif
+
+			// #ifdef PRINT_PRESSURE
+			// 	int finger_idx = PINKY;
+			// 	uint8_t pb_idx = 4*finger_idx;
+			// 	if(pb_idx > 16)
+			// 		pb_idx = 16;
+			// 	int pidx = 0;
+			// 	for(pidx = 0; pidx < 3; pidx++)
+			// 		printf("%.3f, ",(float)pres_fmt.v[pb_idx+pidx]/6553.5f);
+			// 	printf("%.3f\r\n",(float)pres_fmt.v[pb_idx+pidx]/6553.5f);
+			//
+			// #elif defined PRINT_POSITION	//Print the position
+			// 	int ch;
+			// 	for(ch = 0; ch < NUM_CHANNELS-1; ch++)
+			// 		printf("q[%d] = %f, ",ch,i2c_in.v[ch]);
+			// 	printf("q[%d] = %f\r\n",ch,i2c_in.v[ch]);
+			// #else
+			// 	const char * yn[2] = {"on ","off"};
+			// 	for(int ch = 0; ch < NUM_CHANNELS; ch++)
+			// 		printf("%s: %s ", finger_name[ch], yn[((disabled_stat >> ch) & 1)] );
+			// 	printf("\r\n");
+			// #endif
+		}
+		rc = send_recieve_floats(TORQUE_CTL_MODE, &i2c_out, &i2c_in, &disabled_stat, &pres_fmt);
+		print_hr_errcode(rc);
+	}
 }
